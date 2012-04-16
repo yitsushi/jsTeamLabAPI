c6f8ba70567b9e1ecabf144a1291beeab46fed7a
Balazs Nadasdi <yitsushi@gmail.com>
2012-04-17 00:27:32 +0200

    Project:: Write methods + some test

--- test/config.js
+++ test/config.js
@@ -7,3 +7,11 @@ module.exports.true_login = {
   username: 'demouser@mailinator.com',
   password: 'DemoUser'
 };
+
+module.exports.project = {
+  title: "Test Project",
+  description: "Test Project via jsTeamLabAPI",
+  responsibleId: "02a923e8-4eb4-47ce-840d-3e5398b60c5c",
+  tags: "tag, list, api",
+  private: false
+}
--- test/project-test.js
+++ test/project-test.js
@@ -16,6 +16,23 @@ describe('project', function() {
         done(err);
       });
   });
+  
+  describe('#create_project', function() {
+    it('should create a new project and return with details of created project', function(done) {
+      api.project.create_project(
+        Config.project.title,
+        Config.project.description,
+        Config.project.responsibleId,
+        Config.project.tags,
+        Config.project.private,
+        function(err, data) {
+          should.not.exist(err);
+          should.exist(data);
+          project = data;
+          done();
+        })
+    });
+  });
 
   describe('#list', function() {
     it('should return with a list of projects', function(done) {
@@ -23,7 +40,14 @@ describe('project', function() {
         should.not.exist(err);
         should.exist(data);
         assert(typeof data, typeof new Array());
-        project = data[0];
+        var _t = false;
+        for(var i = 0, _l = data.length; i < _l; i++) {
+          if (data[i].id === project.id) {
+            _t = true;
+            break;
+          }
+        }
+        assert(_t, true);
         done();
       });
     });
@@ -46,8 +70,25 @@ describe('project', function() {
         should.not.exist(err);
         should.exist(data);
         assert(typeof data, typeof new Array());
+        var _t = false;
+        for(var i = 0, _l = data.length; i < _l; i++) {
+          if (data[i].id === project.id) {
+            _t = true;
+            break;
+          }
+        }
+        assert(_t, true);
         done();
       });
     });
   });
+  describe('#delete_project', function() {
+    it('should delete the test project', function(done) {
+      api.project.delete_project(project.id, function(err, data) {
+        should.not.exist(err);
+        should.exist(data);
+        done();
+      });
+    });
+  })
 });
